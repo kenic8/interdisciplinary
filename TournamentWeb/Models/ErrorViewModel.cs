6c9c583dd8748daffa1aba5d6f02bb7b75e42b07
KN <nicholas.kevin96@gmail.com>
2021-05-04 12:52:13 +0200

    Clean Project

---
+++ TournamentWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TournamentWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
